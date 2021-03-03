9f67fd5cd70aeaa73110a02d2ae72da8e46b45db
Besi <david.besenyei97@gmail.com>
2021-03-03 17:33:15 +0100

    Add tests

--- src/app/components/devices/devices.component.ts
+++ src/app/components/devices/devices.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import {HttpClient} from '@angular/common/http';
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-devices',
   templateUrl: './devices.component.html',
   styleUrls: ['./devices.component.css']
 })
-export class DevicesComponent {
+export class DevicesComponent implements OnInit {
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
@@ -29,5 +31,14 @@ export class DevicesComponent {
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.http.get('http://api.smarthome.besi.dev/admin/' + firebase.auth().currentUser.uid).subscribe( response => console.log(response) );
+    this.http.get('http://api.smarthome.besi.dev/test').subscribe( response => console.log(response) );
+  }
+
 }
