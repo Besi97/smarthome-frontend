8a32be6423fcc144d3a17375939f5831385a3770
Besi <david.besenyei97@gmail.com>
2021-04-14 18:32:18 +0200

    Implement device service

--- src/app/services/device.service.ts
+++ src/app/services/device.service.ts
@@ -1,27 +1,46 @@
 import {Injectable} from '@angular/core';
 import {SmarthomeService} from './smarthome.service';
 import {Observable} from 'rxjs';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {map} from 'rxjs/operators';
+import firebase from 'firebase';
+import DocumentSnapshot = firebase.firestore.DocumentSnapshot;
+import QuerySnapshot = firebase.firestore.QuerySnapshot;
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeviceService {
 
+  private devicesPath = 'devices';
+  private devicesCollection = 'devices';
+
   constructor(
-    private smarthomeService: SmarthomeService
+    private smarthomeService: SmarthomeService,
+    private firestore: AngularFirestore
   ) {
   }
 
-  // TODO: get device from Firestore
+  getDevice(deviceId: string): Observable<Device> {
+    return this.firestore.collection(this.devicesCollection).doc<Device>(deviceId).get().pipe(
+      map<DocumentSnapshot<Device>, Device>(document => document.data())
+    );
+  }
+
+  listDevices(): Observable<Device[]> {
+    return this.firestore.collection<Device>(this.devicesCollection).get().pipe(
+      map<QuerySnapshot<Device>, Device[]>(query => query.docs.map(document => document.data()))
+    );
+  }
 
   postDevice(type: DeviceType): Observable<Device> {
     const payload: DeviceTypeWrapper = {type};
-    return this.smarthomeService.post<Device>('devices', payload);
+    return this.smarthomeService.post<Device>(this.devicesPath, payload);
   }
 
   updateDeviceName(deviceId: string, newName: string): Observable<Device> {
     const payload: StringWrapper = {data: newName};
-    return this.smarthomeService.put<Device>(`devices/${deviceId}`, payload);
+    return this.smarthomeService.put<Device>(`${this.devicesPath}/${deviceId}/name`, payload);
   }
 
 }
